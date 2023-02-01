122b1cdf63dc48a22d645fc597a44a9ffb0d0b4b
Michael Milton <ttmigueltt@gmail.com>
2023-02-01 17:56:15 +1100

    Update

--- R/hello.R
+++ R/hello.R
@@ -27,18 +27,18 @@ get_version = function(){
     }
 }
 
-#' Asserts that the current EnvironmentModules version is above the minimum
-#' supported version of this package. Raises an error if it is not.
+#' Asserts that the current EnvironmentModules version is above a specified
+#' version. Raises an error if it is not.
+#' @param against The version to check against. By default this is the minimum
+#'  version supported by this package.
 #' @return Invisible
 #' @export
 #' @examples
 #' check_version()
-check_version = function(){
+check_version = function(against = MIN_SUPPORTED_VERSION){
   version = get_version()
-  if (version < MIN_SUPPORTED_VERSION){
-    version |>
-      paste0("Your Environment Modules version is ", version = _, ", which is lower than ", MIN_SUPPORTED_VERSION, ", the minimum supported version.") |>
-      cli::cli_abort()
+  if (version < MIN_SUPPORTED_VERSION || is.null(version)){
+      cli::cli_abort("Your Environment Modules version is {version}, which is lower than {MIN_SUPPORTED_VERSION}, which is required for the function you just ran.")
   }
   invisible()
 }
@@ -71,6 +71,7 @@ get_modulescmd_binary = function(){
 #' @param ... Arguments to forward to [base::system2()]
 #' @return A character scalar containing the command's output
 run_modulecmd = function(args, ...){
+  check_version()
   c("r", args) |> system2(command = get_modulescmd_binary(), args=_, ...)
 }
 
@@ -124,13 +125,51 @@ module_load = function(modules, link_libs = FALSE, dyn_load_args = list(), env =
   }
   else {
     eval(code)
+    cli::cli_alert_success("Successfully loaded {modules}")
   }
 
   if (link_libs){
+    initial_ld = strsplit(initial_ld, ":") |> unlist()
+    final_ld = Sys.getenv("LD_LIBRARY_PATH") |> strsplit(":") |> unlist()
+    setdiff(final_ld, initial_ld) |>
+      list(ld_library_paths=_) |>
+      c(dyn_load_args) |>
+      do.call(link_module_libs, args=_)
+  } else {
+    list()
+  }
+}
+
+#' Unloads one or more environment modules
+#' @param modules A character vector of environment modules to unload
+#' @param unlink_libs A logical scalar. If TRUE, unlink R from all the shared
+#'  libraries contained within that module. This is not the default behaviour.
+#' @inheritParams get_module_code
+#' @return A list of `DLLInfo` objects, containing the libraries (if any)
+#'  that were linked. See [base::getLoadedDLLs] for an explanation of this
+#'  class.
+#' @export
+#' @examples
+#' module_unload("python")
+module_unload = function(modules, unlink_libs = FALSE, env = character(0)){
+  initial_ld = Sys.getenv("LD_LIBRARY_PATH")
+
+  code = c("unload", modules) |> get_module_code(env = env)
+
+  if (length(code) == 0){
+    cli::cli_alert_info("Nothing to do. This module was probably not loaded. Use module_list() to verify.")
+  }
+  else {
+    eval(code)
+    cli::cli_alert_success("Successfully unloaded {modules}")
+  }
+
+  if (unlink_libs){
     initial_ld = strsplit(initial_ld, ":")
     final_ld = Sys.getenv("LD_LIBRARY_PATH") |> strsplit(":")
     setdiff(initial_ld, final_ld) |>
       do.call(link_module_libs, dyn_load_args)
+    Invisible()
   } else {
     list()
   }
@@ -140,12 +179,19 @@ module_load = function(modules, link_libs = FALSE, dyn_load_args = list(), env =
 #' @return The same format as [get_module_output()]
 #' @param starts_with An optional character scalar. If provided, only modules
 #' whose name starts with this character string will be returned.
+#' @param contains An optional character scalar. This parameter is only
+#' supported in Environment Modules 4.3+. If provided, it will filter modules
+#' to only those containing this substring.
 #' @inheritDotParams get_module_output
 #' @export
 #' @examples
 #' module_list() |> cat()
-module_list = function(starts_with = NULL, ...){
-  c("list", starts_with) |> get_module_output(...)
+module_list = function(starts_with = NULL, contains = NULL, ...){
+  args = c("list", starts_with)
+  if (!is.null(contains)){
+    args = c(args, "--contains", contains)
+  }
+  get_module_output(args, ...)
 }
 
 #' Unloads all modules that are currently loaded
@@ -176,8 +222,12 @@ module_swap = function(from, to){
 #' @export
 #' @examples
 #' module_avail() |> cat()
-module_avail = function(starts_with = NULL, ...){
-  c("avail", starts_with) |> get_module_output(...)
+module_avail = function(starts_with = NULL, contains = NULL, ...){
+  args = c("avail", starts_with)
+  if (!is.null(contains)){
+    args = c(args, "--contains", contains)
+  }
+  get_module_output(args = args, ...)
 }
 
 #' Links R to the shared libraries in a number of directories
@@ -196,5 +246,28 @@ link_module_libs = function(ld_library_paths, ...){
     list.files(pattern = ".*\\.so", full.names = TRUE) |>
     normalizePath() |>
     unique() |>
-    lapply(dyn.load, ...)
+    lapply(\(lib){
+      dyn.load(lib, now=FALSE, local=FALSE) |> try()
+    })
+}
+
+load_topologically = function(libs){
+  deps = ldd(libs) |> suppressWarnings()
+  deps = deps |>
+    lapply(\(subdeps){
+      names(deps) |>
+        intersect(subdeps)
+    })
+
+  dep_df = data.frame(paths = libs, deps = I(deps), symbols = names(deps))
+
+  while(TRUE){
+    to_remove = lengths(dep_df$deps) == 0
+    dep_df[to_remove, "paths"] |> lapply(dyn.load)
+    dep_df = def_df[-to_remove, ]
+  }
+
+  lapply(libs, \(dll){
+    dyn.load(dll, ..., now = FALSE, local = FALSE) |> try()
+  })
 }
